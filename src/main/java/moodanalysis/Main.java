865e67833276e31dbf3e47d3a37868083b81680a
Amit <amitlpalandurkar1998@gmail.com>
2023-07-14 01:16:03 +0530

    Junit Testing Mood Analyzer Problem project File Structure.

---
+++ src/main/java/moodanalysis/Main.java
@@ -0,0 +1,7 @@
+package moodanalysis;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wellcome To Junit Testing of Mood Analyzer Problem.");
+    }
+}
---
+++ src/test/java/TestMood.java
@@ -0,0 +1,5 @@
+public class TestMood {
+    public static void main(String[] args) {
+
+    }
+}
